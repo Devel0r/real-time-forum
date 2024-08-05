a93543707e4746d2dcf097ac2428e0a01884a4fa
dpruel <dvpruel@gmail.com>
2024-08-05 20:40:37 +0300

    feat(logger): Writing Logger

--- cmd/real-time-forum/main.go
+++ cmd/real-time-forum/main.go
@@ -1,15 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log/slog"
+
+	"github.com/Pruel/real-time-forum/pkg/config"
+	"github.com/Pruel/real-time-forum/pkg/logger"
+)
 
 func main() {
-    // TODO: configuration
+	// TODO: configuration
+	cfg, err := config.InitConfig(); 
+    if err != nil {
+        slog.Error(err.Error())
+        return
+    }
 
     // TODO: level loggining
+    if _, err := logger.InitLogger(cfg); err != nil {
+        slog.Error(err.Error())
+    } 
 
-    // TODO: App.Run, run the app logic with error handling
+	// TODO: App.Run, run the app logic with error handling
+    
 
-    // TODO: App.Stop with error handling
+	// TODO: App.Stop with error handling
 
-    fmt.Print("Hello world")
+	fmt.Print("Hello world")
 }
---
+++ pkg/logger/logger.go
@@ -0,0 +1,25 @@
+package logger
+
+import (
+	"log/slog"
+	"os"
+
+	"github.com/Pruel/real-time-forum/pkg/cstructs"
+	"github.com/Pruel/real-time-forum/pkg/validator"
+)
+
+func InitLogger(cfg *cstructs.Config) (logger *slog.Logger, err error) {
+	if err := validator.ValidateConfigParams(cfg); err != nil {
+		return nil, err 
+	}
+
+	logger = slog.New(
+		slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+		Level: slog.Level(cfg.Logger.Level), 
+		AddSource: cfg.Logger.SourceKey,
+	}).WithAttrs([]slog.Attr{
+		slog.String("service_name", cfg.ServiceName)}))
+	
+	return logger, nil
+}
+
