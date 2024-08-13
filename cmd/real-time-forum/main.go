61a24899daaac727b1294fefc2cce0cdd39b0aa9
dpruel <dvpruel@gmail.com>
2024-08-13 20:28:34 +0300

    chore: update dependencies, modify main configuration

--- cmd/real-time-forum/main.go
+++ cmd/real-time-forum/main.go
@@ -2,20 +2,24 @@ package main
 
 import (
 	"log/slog"
+	"os"
 
+	"github.com/Pruel/real-time-forum/internal/app"
 	"github.com/Pruel/real-time-forum/pkg/config"
 	"github.com/Pruel/real-time-forum/pkg/logger"
 )
 
 func main() {
-	// TODO: configuration
+	slog.Info("Real Time Forum running...")
+	// configuration
 	cfg, err := config.InitConfig()
 	if err != nil {
 		slog.Error(err.Error()) // log with error level// slog.Debug, Info -> os.Exit()
 		// log.Fatal() -> // fmt.Println(), os.Exit(1) - process crashed
-		// os.Exit(1)// os -> sys call -> exit ->
+		os.Exit(1) // os -> sys call -> exit ->
 		return
 	}
+	slog.Debug("Configuration successful initialized")
 
 	// error, panic() -> defer func() { // ok
 	// 	if r := recover(); r != nil {
@@ -23,16 +27,19 @@ func main() {
 	// 	}
 	// }
 
-	// TODO: level loggining
+	// level loggining
 	if _, err := logger.InitLogger(cfg); err != nil {
 		slog.Error(err.Error())
 		return
 	}
+	slog.Debug("Logger successful initialized")
 
-	// TODO: App.Run, run the app logic with error handling
+	slog.Info("Real Time Forum started!")
+	// App.Run, run the app logic with error handling
+	if err := app.Run(cfg); err != nil {
+		slog.Error(err.Error())
+	}
 
-	// TODO: App.Stop with error handling
-
-	println("forum service")
+	// App.Stop with error handling
 
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/Pruel/real-time-forum
 
 go 1.21.7
 
-require gopkg.in/yaml.v2 v2.4.0 // indirect
+require (
+	github.com/mattn/go-sqlite3 v1.14.22
+	gopkg.in/yaml.v2 v2.4.0
+)
